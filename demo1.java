5f424107f2ba57482f70336d72735589b19854c3
lihaidehaoren <529509927@qq.com>
2022-11-11 20:54:14 +0800

    Update demo1.java

--- demo1.java
+++ demo1.java
@@ -28,6 +28,35 @@ public class demo1 {
                 System.out.print(string2[i][j]+" ");
             }
             System.out.println();
+            int[][] string3=new int[row][col];
+
+            if (row==2){
+                string3[0][0]=string1[0][0]+string2[0][0];
+                string3[0][1]=string1[0][1]*string2[0][1];
+                string3[1][0]=string1[1][0]*string2[1][0];
+                string3[1][1]=string1[1][1]+string2[1][1];
+            }
+            else if (row==4){
+                string3[0][0]=string1[0][0]+string2[0][0];
+                string3[0][1]=string1[0][1]+string2[0][1];
+                string3[1][0]=string1[1][0]+string2[1][0];
+                string3[1][1]=string1[1][1]+string2[1][1];
+
+                string3[2][2]=string1[2][2]-string2[2][2];
+                string3[2][3]=string1[2][3]-string2[2][3];
+                string3[3][2]=string1[3][2]-string2[3][2];
+                string3[3][3]=string1[3][3]-string2[3][3];
+
+                string3[0][2]=string1[0][2]*string2[0][2]+string1[0][3]*string2[0][3];
+                string3[1][2]=string1[1][2]*string2[0][2]+string1[1][3]*string2[0][3];
+                string3[0][3]=string1[0][2]*string2[1][2]+string1[0][3]*string2[1][3];
+                string3[1][3]=string1[1][2]*string2[1][2]+string1[1][3]*string2[1][3];
+
+                string3[2][0]=string2[2][0]*string1[2][0]+string2[2][1]*string1[2][1];
+                string3[3][0]=string2[3][0]*string1[2][0]+string2[3][1]*string1[2][1];
+                string3[2][1]=string2[2][0]*string1[3][0]+string2[2][1]*string1[3][1];
+                string3[3][1]=string2[3][0]*string1[3][0]+string2[3][1]*string1[3][1];
+            }
         }
     }
 }
